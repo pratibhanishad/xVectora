817002fd4c00f73c8fed5450d392a051b033b7d0
Pratibha Nishad <meeraji634@gmail.com>
2026-05-26 10:15:14 +0530

    Add GitHub Actions CI pipeline

--- packages/domain/src/index.js
+++ packages/domain/src/index.js
@@ -1,4 +1,17 @@
-const { User } = require('./User');
+class User {
+  constructor({ id, name, email }) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+  validate() {
+    if (!this.email.includes('@')) {
+      throw new Error('Invalid email');
+    }
+    return true;
+  }
+}
+
 const { AssetRepositoryPort } = require('./ports/AssetRepositoryPort');
 const { PredictionRunnerPort } = require('./ports/PredictionRunnerPort');
 const { RecommendationRepositoryPort } = require('./ports/RecommendationRepositoryPort');
